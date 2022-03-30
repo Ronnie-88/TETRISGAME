d28d97e94405fdf1516d867571d661638584dc35
Ronald <kamangaronald@gmail.com>
2022-03-30 09:57:35 -0700

    Refactored Tetramnio

--- 4DTetris/Tetramino.cpp
+++ 4DTetris/Tetramino.cpp
@@ -142,21 +142,25 @@ const bool& Tetramino::DoesBlockFit(const int* currentGrid, const int* gameGrid,
 				{
 					canBlockFit = false;
 					return canBlockFit;
+					//return !canBlockFit;
 				}
 				if ((potentialMovePos.x + x) > (gridwidth - 2))//right side of grid
 				{
 					canBlockFit = false;
 					return canBlockFit;
+					//return !canBlockFit;
 				}
 				if ((potentialMovePos.y + y) > (gridheight - 2))//base of grid
 				{
 					canBlockFit = false;
 					return canBlockFit;
+					//return canBlockFit;
 				}
 				if (gameGrid[(potentialMovePos.y + y) * (gridwidth)+(potentialMovePos.x + x)] == 1)//collision with another block
 				{
 					canBlockFit = false;
 					return canBlockFit;
+					//return canBlockFit;
 				}
 			}
 
@@ -164,6 +168,7 @@ const bool& Tetramino::DoesBlockFit(const int* currentGrid, const int* gameGrid,
 	}
 	canBlockFit = true;
 	return canBlockFit;
+	//return canBlockFit;
 }
 
 const bool& Tetramino::DoesRotatedBlockFit(const int* gameGrid, const int& gridheight, const int& gridwidth)
